8ce56aea048be67c9b36ec3331c07d7104fc1c25
josepipher <josepipher@users.noreply.github.com>
2019-10-25 10:18:48 -0700

    Update randrw-400.sh

--- randrw-400.sh
+++ randrw-400.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # random read/write performance
-bash fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=400G --readwrite=randrw --rwmixread=25
+cd fio*
+./fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=400G --readwrite=randrw --rwmixread=25
